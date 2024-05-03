35d0596f439eed6614ccba736ec8d771c593c40b
Louison <61231354+TheLokj@users.noreply.github.com>
2024-05-03 14:13:53 +0100

    Update bin/parse_dmc_classification.py
    Co-authored-by: Varsha Kale <34109092+Vkale1@users.noreply.github.com>

--- bin/parse_dmc_classification.py
+++ bin/parse_dmc_classification.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 
-if " " in sys.argv[1] :
-  seqName = sys.argv[1].split(" ")[0]
-else :
-  seqName = sys.argv[1]
+seq_name = sys.argv[1].split(" ", 1)[0]
 
 if float(sys.argv[2]) != 0 and float(sys.argv[3]) != 0 and float(sys.argv[4]) != 0 and float(sys.argv[5]) != 0 and float(sys.argv[6]) != 0 : 
   print(f'{seqName}\t{["Eukaryote", "EukaryoteVirus", "Plasmid", "Prokaryote", "ProkaryoteVirus"][[float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6])].index(max(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6])))]}')
