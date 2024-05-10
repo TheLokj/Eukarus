f98bb0cb8abd68e848d709d4ed94595f1a6f234c
Louison Tristan Lilian Lesage <61231354+TheLokj@users.noreply.github.com>
2024-05-10 17:34:03 +0100

    parse_dmc_classification.py correction

--- bin/parse_dmc_classification.py
+++ bin/parse_dmc_classification.py
@@ -8,6 +8,6 @@ dmcValues = [float(arg) for arg in sys.argv[2:7]]
 if 0 not in dmcValues:
   maxIndex = dmcValues.index(max(dmcValues))
   labels = ["Eukaryote", "EukaryoteVirus", "Plasmid", "Prokaryote", "ProkaryoteVirus"]
-  print(f'{seqName}\t{labels[maxIndex]}'
+  print(f'{seqName}\t{labels[maxIndex]}')
 else :
   print(f'{seqName}\tUnknown')
